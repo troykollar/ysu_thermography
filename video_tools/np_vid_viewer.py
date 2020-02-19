f37040eb36f5e105aea3fdd743bc07de4e512b3e
troy <troykollar@gmail.com>
2020-02-19 13:31:45 -0500

    Removed find_lower_bounds from np_vid_viewer

--- video_tools/np_vid_viewer.py
+++ video_tools/np_vid_viewer.py
@@ -31,7 +31,7 @@ class NpVidTool:
     """
     def __init__(self,
                  window_name="Video",
-                 mp_data_on_img=False,
+                 mp_data_on_vid=False,
                  remove_reflection=False,
                  remove_lower=False):
         """Create an NpVidViewer Object.
@@ -40,6 +40,8 @@ class NpVidTool:
         ----------
         window_name : str
             Name of the window that the video will be displayed in.
+        mp_data_on_vid : bool
+            Add meltpool data on top of the video if true
         remove_reflection : bool
             Run the remove reflection function if true.
         remove_lower : bool
@@ -51,7 +53,7 @@ class NpVidTool:
         self.window_name = window_name
 
         self.max_temp = []
-        self.mp_data_on_img = mp_data_on_img
+        self.mp_data_on_vid = mp_data_on_vid
 
     def generate_video(self, save_video=False):
         Tk().withdraw  #prevent tkinter from opening root window
@@ -101,7 +103,7 @@ class NpVidTool:
             img = cv2.applyColorMap(img, cv2.COLORMAP_INFERNO)
 
             # Add meltpool data onto the image
-            if self.mp_data_on_img:
+            if self.mp_data_on_vid:
                 self.add_mp_data_to_img(img, i)
 
             # Add image to video array
@@ -124,50 +126,6 @@ class NpVidTool:
             cv2.waitKey(waitKey)
         cv2.destroyAllWindows()
 
-    def find_lower_bounds(self):
-        """Find the lower bounds of the piece.
-
-        Returns
-        -------
-        max_locations
-            List of the first points at the zero level below the each max temp.
-        """
-        img_array = self.temp_data
-        i = 0
-        # Find the x and y value of the max temp of first frame
-        max_x = np.where(img_array[0] == np.amax(img_array[0]))[1][0]
-        max_y = np.where(img_array[0] == np.amax(img_array[0]))[0][0]
-
-        # Find the x value of the max temp of the next frame
-        next_max_x = np.where(img_array[1] == np.amax(img_array[1]))[1][0]
-
-        # Create lists of the x and y values of the max temperatures
-        max_x_locations = []
-        max_y_locations = []
-
-        # While the laser is moving to the right. (i.e. the next location > current location)
-        while max_x < next_max_x:
-            max_x_locations.append([i, max_x])
-            max_y_locations.append([i, max_y])
-            i = i + 1
-            max_x = np.where(img_array[i] == np.amax(img_array[i]))[1][0]
-            next_max_x = np.where(
-                img_array[i + 1] == np.amax(img_array[i + 1]))[1][0]
-            max_y = np.where(img_array[i] == np.amax(img_array[i]))[0][0]
-
-        for frame in range(0, len(max_y_locations)):
-            while img_array[frame, max_y_locations[frame][1],
-                            max_x_locations[frame][1]] > 174:
-                max_y_locations[frame][1] += 1
-
-        max_locations = []
-        j = 0
-        for i in range(max_x_locations[0][1], max_x_locations[-1][1]):
-            if i > max_x_locations[j][1]:
-                j = j + 1
-            max_locations.append((i, max_y_locations[j][1]))
-        return max_locations
-
     def match_vid_to_meltpool(self):
         """Shuffle the meltpool data and thermal camera data together based on timestamp"""
         self.matched_array = []
@@ -185,45 +143,6 @@ class NpVidTool:
                 self.meltpool_data[self.mp_data_index][3],
                 self.meltpool_data[self.mp_data_index][4]
             ])
-
-    def save_video(self, filename="Video.avi", framerate=60):
-        """Generate video based on `temp_data` information, and save under `filename`.
-
-        Parameters
-        ----------
-        filename : str
-            Name to save the video file as.
-        framerate : int
-            Framerate to generate the video with.
-        """
-        height = self.temp_data[0].shape[0]
-        width = self.temp_data[0].shape[1]
-        size = (width, height)
-        out = cv2.VideoWriter(filename,
-                              cv2.VideoWriter_fourcc("f", "m", "p", "4"),
-                              framerate, size)
-        for i in range(0, self.temp_data.shape[0]):
-            percent = (i / self.temp_data.shape[0]) * 100
-            print("Saving video: " + str("%.2f" % percent) + "%")
-            img = self.temp_data[i]
-            normalized_img = img.copy()
-            if self.remove_reflection:
-                ReflectionRemover.remove(normalized_img,
-                                         zero_level_threshold=180,
-                                         max_temp_threshold=700,
-                                         remove_lower=self._remove_lower,
-                                         lower_bounds=self.lower_bounds)
-
-            normalized_img = cv2.normalize(normalized_img, normalized_img, 0,
-                                           255, cv2.NORM_MINMAX, cv2.CV_8UC1)
-            normalized_img = cv2.applyColorMap(normalized_img,
-                                               cv2.COLORMAP_INFERNO)
-
-            self.add_mp_data_to_img(normalized_img, i)
-
-            out.write(normalized_img)
-
-        out.release()
 
     def video_timestamp(self, frame):
         """Return the timestamp of the video based on the frame."""
@@ -262,41 +181,6 @@ class NpVidTool:
             "| MP Z: " + str(self.mp_z(frame)),
             "| MP Area: " + str(self.mp_area(frame)),
         )
-
-    def update_image(self, frame: int):
-        """Normalize the array data. Apply the colormap, and add meltpool data to the image.
-
-        Parameters
-        ----------
-        frame : int
-            The current frame of the video
-        Returns
-        -------
-        normalized_img
-            Numpy array of the updated image of the current frame.
-        """
-        img = self.temp_data[frame]
-        self.max_temp.append(np.amax(self.temp_data[frame]))
-        normalized_img = img.copy()
-        if self.remove_reflection:
-            reflection_remover.ReflectionRemover.remove(
-                normalized_img,
-                zero_level_threshold=180,
-                max_temp_threshold=700,
-                remove_lower=self._remove_lower,
-                lower_bounds=self.lower_bounds,
-            )
-
-        normalized_img = cv2.normalize(normalized_img, normalized_img, 0, 255,
-                                       cv2.NORM_MINMAX, cv2.CV_8UC1)
-        normalized_img = cv2.applyColorMap(normalized_img,
-                                           cv2.COLORMAP_INFERNO)
-
-        self.add_mp_data_to_img(normalized_img, frame)
-        self.highlight_max_temp(frame, normalized_img)
-
-        self.print_info(frame)
-        return normalized_img
 
     def highlight_max_temp(self, frame, img):
         max_temp = self.max_temp[frame]
