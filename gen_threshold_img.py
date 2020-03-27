f5830cccdab09ed54ea2821ba89578db43275f2d
Troy Kollar <troykollar@gmail.com>
2020-03-27 14:40:01 -0400

    Updated gen_threshold_img formatting

--- gen_threshold_img.py
+++ gen_threshold_img.py
@@ -1,18 +1,18 @@
+"""Generates a thermal image based on the number of times each pixel exceeds a temperature threshold"""
 import argparse
-import np_vid_viewer
 import np_vid_viewer.composite as composite
 
-parser = argparse.ArgumentParser(description='Analyze temperature data.')
-parser.add_argument('temp_data',
+PARSER = argparse.ArgumentParser(description='Analyze temperature data.')
+PARSER.add_argument('temp_data',
                     type=str,
                     help='the file location/name of the temperature data')
-parser.add_argument('threshold',
+PARSER.add_argument('THRESHOLD',
                     type=int,
                     help='temperature theshold of the image')
 
-args = parser.parse_args()
+ARGS = PARSER.parse_args()
 
-temp_data_file = args.temp_data
-threshold = args.threshold
+TEMP_DATA_FILE = ARGS.temp_data
+THRESHOLD = ARGS.THRESHOLD
 
-composite.generate_threshold_image(temp_data_file, threshold=threshold)
+composite.generate_threshold_image(TEMP_DATA_FILE, threshold=THRESHOLD)
