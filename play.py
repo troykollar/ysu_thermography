a718e6c7de0ac0e9fc8e53d462be391edfb6e248
Troy Kollar <troykollar@gmail.com>
2020-03-28 23:15:35 -0400

    play.py now prints an error message if there is a problem with the input for temp_data and/or merged_data

--- play.py
+++ play.py
@@ -1,19 +1,36 @@
+"""Uses NpVidTool to generate and play a video with cv2.imshow()"""
 import argparse
 import np_vid_viewer
 
-parser = argparse.ArgumentParser(description='Analyze temperature data.')
-parser.add_argument('temp_data',
+
+def get_extension(filename: str):
+    """Returns the file extensino of a given filename."""
+    return filename[filename.rfind('.'):]
+
+
+def validate_extension(argument_name: str, filename: str,
+                       required_extension: str):
+    """Returns true if the extension of a given filename does not equal the required_extension"""
+    error = False
+    if get_extension(filename) != required_extension:
+        print(argument_name + " must be of type '" + required_extension + "'")
+        error = True
+    return error
+
+
+PARSER = argparse.ArgumentParser(description='Analyze temperature data.')
+PARSER.add_argument('temp_data',
                     type=str,
                     help='the file location/name of the temperature data')
-parser.add_argument('merged_data',
+PARSER.add_argument('merged_data',
                     type=str,
                     help='the file location/name of the merged data')
-parser.add_argument('-scale',
+PARSER.add_argument('-scale',
                     type=int,
                     default=1,
                     required=False,
                     help='int factor to scale output videos by')
-parser.add_argument(
+PARSER.add_argument(
     '-delay',
     type=int,
     default=1,
@@ -21,26 +38,26 @@ parser.add_argument(
     help=
     'int specifying millisecond delay between showing each frame in play_video()'
 )
-parser.add_argument(
+PARSER.add_argument(
     '-mp',
     type=int,
     default=False,
     required=False,
     help='0 or 1 specifying wether or not to overlay meltpool data on the image'
 )
-parser.add_argument(
+PARSER.add_argument(
     '-top',
     type=int,
     default=False,
     required=False,
     help='0 or 1 specifying wether or not to remove top reflections')
-parser.add_argument(
+PARSER.add_argument(
     '-bot',
     type=int,
     default=False,
     required=False,
     help='0 or 1 specifying wether or not to remove bottom reflections')
-parser.add_argument(
+PARSER.add_argument(
     '-showmax',
     type=int,
     default=False,
@@ -48,22 +65,35 @@ parser.add_argument(
     help=
     '0 or 1 specifying whether or not to highlight the max temp of the frame')
 
-args = parser.parse_args()
+ARGS = PARSER.parse_args()
 
-temp_data_file = args.temp_data
-merged_data_file = args.merged_data
-scale_factor = args.scale
-frame_delay = args.delay
-top = bool(args.top)
-bot = bool(args.bot)
-show_max = bool(args.showmax)
+ARGUMENT_ERROR = False
+# Validate temp_data file input
+TEMP_DATA_FILE = ARGS.temp_data
+TEMP_DATA_ERROR = validate_extension('temp_data', TEMP_DATA_FILE, '.npy')
 
-VIEWER = np_vid_viewer.NpVidTool(temp_filename=temp_data_file,
-                                 data_filename=merged_data_file,
-                                 scale_factor=scale_factor,
-                                 mp_data_on_vid=args.mp,
-                                 remove_top_reflection=top,
-                                 remove_bottom_reflection=bot,
-                                 circle_max_temp=show_max)
+# Validate merged_data file input
+MERGED_DATA_FILE = ARGS.merged_data
+MERGED_DATA_ERROR = validate_extension('merged_data', TEMP_DATA_FILE, '.npy')
 
-VIEWER.play_video(frame_delay)
+ARGUMENT_ERROR = MERGED_DATA_ERROR or TEMP_DATA_ERROR
+
+SCALE_FACTOR = ARGS.scale
+FRAME_DELAY = ARGS.delay
+REMOVE_TOP_REFLECTION = bool(ARGS.top)
+REMOVE_BOTTOM_REFLECTION = bool(ARGS.bot)
+SHOW_MAX = bool(ARGS.showmax)
+
+if ARGUMENT_ERROR:
+    pass
+else:
+    VIEWER = np_vid_viewer.NpVidTool(
+        temp_filename=TEMP_DATA_FILE,
+        data_filename=MERGED_DATA_FILE,
+        scale_factor=SCALE_FACTOR,
+        mp_data_on_vid=ARGS.mp,
+        remove_top_reflection=REMOVE_TOP_REFLECTION,
+        remove_bottom_reflection=REMOVE_BOTTOM_REFLECTION,
+        circle_max_temp=SHOW_MAX)
+
+    VIEWER.play_video(FRAME_DELAY)
