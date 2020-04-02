b69276419c5d05fed97999f9a6e0132c83ad9cc5
Troy Kollar <troykollar@gmail.com>
2020-04-02 08:27:15 -0400

    Updated save_frame_range.py to use new constructor which takes only the data directory

--- save_frame_range.py
+++ save_frame_range.py
@@ -3,12 +3,9 @@ import np_vid_viewer
 import argparse
 
 parser = argparse.ArgumentParser(description='Analyze temperature data.')
-parser.add_argument('temp_data',
+parser.add_argument('data_directory',
                     type=str,
-                    help='the file location/name of the temperature data')
-parser.add_argument('merged_data',
-                    type=str,
-                    help='the file location/name of the merged data')
+                    help='the location of the data')
 parser.add_argument('start', type=int, help='first frame to save')
 parser.add_argument('end', type=int, help='last frame to save')
 parser.add_argument('-scale',
@@ -19,14 +16,12 @@ parser.add_argument('-scale',
 
 args = parser.parse_args()
 
-temp_data_file = args.temp_data
-merged_data_file = args.merged_data
+DATA_DIRECTORY = args.data_directory
 scale_factor = args.scale
 start = args.start
 end = args.end
 
-VIEWER = np_vid_viewer.NpVidTool(temp_filename=temp_data_file,
-                                 data_filename=merged_data_file,
+VIEWER = np_vid_viewer.NpVidTool(data_directory=DATA_DIRECTORY,
                                  scale_factor=scale_factor)
 
 VIEWER.save_frame_range16(start, end)
