2e4b92d0c46028cd25a3f224e0cf6909ee4ebb0e
Troy Kollar <troykollar@gmail.com>
2020-04-16 13:28:40 -0400

    Made dataset class to be used for simpler data manipulation

--- np_vid_viewer/__init__.py
+++ np_vid_viewer/__init__.py
@@ -2,3 +2,4 @@ from np_vid_viewer.np_vid_viewer import NpVidTool
 from np_vid_viewer import reflection_remover
 from np_vid_viewer import progress_bar
 from np_vid_viewer import helper_functions
+from np_vid_viewer import dataset
