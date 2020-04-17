1b7ab040f78044351835e468d08639a9eb9255e8
Troy Kollar <troykollar@gmail.com>
2020-04-16 21:05:46 -0400

    Updated save.py to use new project file structure

--- save.py
+++ save.py
@@ -1,66 +1,83 @@
 import argparse
 import np_vid_viewer
+import np_vid_viewer.dataset as dset
 
 parser = argparse.ArgumentParser(description='Analyze temperature data.')
 parser.add_argument('data_directory',
                     type=str,
                     help='the location of the data')
-parser.add_argument('-framerate',
+PARSER = argparse.ArgumentParser(description='Analyze temperature data.')
+PARSER.add_argument('data_directory',
+                    type=str,
+                    help='the file location/name of the temperature data')
+PARSER.add_argument('-fps',
                     type=int,
                     default=60,
                     required=False,
-                    help='Framerate of the video to be saved.')
-parser.add_argument(
-    '-follow', type=int, default=False, required=False, help='0 or 1 specifying wether or not to follow the meltpool in the video'
-)
-parser.add_argument('-scale',
+                    help='video framerate')
+PARSER.add_argument('-scale',
                     type=int,
                     default=1,
                     required=False,
                     help='int factor to scale output videos by')
-parser.add_argument(
+PARSER.add_argument(
+    '-fcontour',
+    type=int,
+    default=False,
+    required=False,
+    help=
+    'integer specifying the number of pixels around the contour cog to focus the image on'
+)
+PARSER.add_argument(
+    '-fmax',
+    type=int,
+    default=False,
+    required=False,
+    help=
+    'integer specifying the number of pixels around the max temperature to focus the image on'
+)
+PARSER.add_argument(
+    '-cthresh',
+    type=int,
+    default=0,
+    required=False,
+    help='Integer specifying the threshold to draw contours from')
+PARSER.add_argument(
     '-mp',
     type=int,
     default=False,
     required=False,
     help='0 or 1 specifying wether or not to overlay meltpool data on the image'
 )
-parser.add_argument(
+PARSER.add_argument(
     '-top',
     type=int,
     default=False,
     required=False,
     help='0 or 1 specifying wether or not to remove top reflections')
-parser.add_argument(
+PARSER.add_argument(
     '-bot',
     type=int,
     default=False,
     required=False,
     help='0 or 1 specifying wether or not to remove bottom reflections')
-parser.add_argument(
-    '-showmax',
-    type=int,
-    default=False,
-    required=False,
-    help=
-    '0 or 1 specifying whether or not to highlight the max temp of the frame')
 
-args = parser.parse_args()
+ARGS = PARSER.parse_args()
 
-DATA_DIRECTORY = args.data_directory
-scale_factor = args.scale
-top = bool(args.top)
-bot = bool(args.bot)
-show_max = bool(args.showmax)
-FRAMERATE = args.framerate
-FOLLOW = bool(args.follow)
+DATA_DIRECTORY = ARGS.data_directory
+SCALE_FACTOR = ARGS.scale
+FPS = ARGS.fps
+REMOVE_TOP_REFLECTION = bool(ARGS.top)
+REMOVE_BOTTOM_REFLECTION = bool(ARGS.bot)
+FOLLOW_MAX = ARGS.fmax
+FOLLOW_CONTOUR = ARGS.fcontour
+CONTOUR_THRESHOLD = ARGS.cthresh
 
-VIEWER = np_vid_viewer.NpVidTool(data_directory=DATA_DIRECTORY,
-                                 scale_factor=scale_factor,
-                                 mp_data_on_vid=args.mp,
-                                 remove_top_reflection=top,
-                                 remove_bottom_reflection=bot,
-                                 circle_max_temp=show_max,
-                                 follow_meltpool=FOLLOW)
+DATASET = dset(DATA_DIRECTORY, REMOVE_TOP_REFLECTION, REMOVE_BOTTOM_REFLECTION)
+VIEWER = np_vid_viewer.data_video(DATASET,
+                                  ARGS.mp,
+                                  follow_max_temp=FOLLOW_MAX,
+                                  contour_threshold=CONTOUR_THRESHOLD,
+                                  follow_contour=FOLLOW_CONTOUR)
 
-VIEWER.save_video(framerate=FRAMERATE)
+VIEWER.save_video(SCALE_FACTOR, FPS)
