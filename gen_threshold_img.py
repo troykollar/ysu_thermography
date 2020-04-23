a68d2ba63415c2cde6500f6335a921128a4c00de
Troy Kollar <troykollar@gmail.com>
2020-04-23 12:52:01 -0400

    Updated threshold image driver file to be able to use start and end arguments

--- gen_threshold_img.py
+++ gen_threshold_img.py
@@ -1,6 +1,6 @@
 """Generates a thermal image based on the number of times each pixel exceeds a temperature threshold"""
 import argparse
-import np_vid_viewer.composite as composite
+from np_vid_viewer.composite import generate_threshold_image
 
 PARSER = argparse.ArgumentParser(description='Analyze temperature data.')
 PARSER.add_argument('temp_data',
@@ -9,10 +9,25 @@ PARSER.add_argument('temp_data',
 PARSER.add_argument('THRESHOLD',
                     type=int,
                     help='temperature theshold of the image')
+PARSER.add_argument('-start',
+                    type=int,
+                    default=0,
+                    required=False,
+                    help='First frame to consider.')
+PARSER.add_argument('-end',
+                    type=int,
+                    default=0,
+                    required=False,
+                    help='Last frame to consider.')
 
 ARGS = PARSER.parse_args()
 
 TEMP_DATA_FILE = ARGS.temp_data
 THRESHOLD = ARGS.THRESHOLD
+START = ARGS.start
+END = ARGS.end
 
-composite.generate_threshold_image(TEMP_DATA_FILE, threshold=THRESHOLD)
+generate_threshold_image(TEMP_DATA_FILE,
+                         threshold=THRESHOLD,
+                         start=START,
+                         end=END)
