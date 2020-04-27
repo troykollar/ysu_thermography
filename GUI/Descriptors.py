7a3eb0823e5fe890b5f9150df063dc2fa406445c
rjyarwood <rjyarwood@student.ysu.edu>
2020-04-26 23:47:05 -0400

    Working on dictionaries

--- GUI/Descriptors.py
+++ GUI/Descriptors.py
@@ -1,8 +1,20 @@
 
-def createFunctionDescriptorTable(frame):
 
+def getHintTextFunctionFrame(frame):
     FUNCTIONPANEL = {
-
-
+        'genThresholdImgLabel': "Generate an image displaying how often each pixel surpassed a certain temperature "
+                                "threshold",
+        'saveFrame': "Save a particular frame in 16 bit color",
+        'playVideoLabel': "Play the given build as a video",
+        'saveVideoLabel': "Save the given build as a video",
+        'gradientHistogram': "Display the gradient values of a given pixel in the form of a histogram",
+        'pixelTempRangeLabel': "Displays a line chart for a given pixels thermal history"
     }
     return FUNCTIONPANEL.get(frame, " ")
+
+def getHintTextThresholdFrame(frame):
+    THRESHOLDFRAME = {
+        'thresholdFrame': "Minimum Temperature to be tracked"
+    }
+    return THRESHOLDFRAME.get(frame, " ")
+
