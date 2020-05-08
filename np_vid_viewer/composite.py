99db0f5e56618c1a0be58c34243285757d57d25d
Troy Kollar <troykollar@gmail.com>
2020-05-08 10:22:23 -0400

    Fixed several import statements

--- np_vid_viewer/composite.py
+++ np_vid_viewer/composite.py
@@ -2,60 +2,77 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-import np_vid_viewer.progress_bar as progress_bar
+import time
+from np_vid_viewer.helper_functions import printProgressBar
 
 
-def generate_threshold_image(temp_filename: str,
-                             dst_folder=None,
-                             threshold=800,
-                             start=0,
-                             end=0):
-    """Saves a 16 bit threshold image
+def increment_from_thresh(img: np.ndarray, data_frame: np.ndarray,
+                          threshold: int):
+    over_thresh_array = cv2.threshold(data_frame, threshold, 1,
+                                      cv2.THRESH_BINARY)
+    over_thresh_array = over_thresh_array[1]
+    img += over_thresh_array
 
-    Each pixel of the resulting image is incremented every time the temperature at that pixel is
-    above the temperature threshold.
 
-    Parameters
-    ----------
-    temp_filename : str
-        Filename including location of the thermal_cam_temps.npy file.
-    threshold : int
-        Temperature threshold which determines if a pixel will be incremented.
+def get_threshold_img(dataset: np.ndarray,
+                      threshold: int,
+                      start=0,
+                      end=0,
+                      cap=None,
+                      show_progress=False):
+    # Get frame size info
+    height = dataset[0].shape[0]
+    width = dataset[0].shape[1]
+    last_frame = dataset.shape[0]
 
-    """
-    # Get temp data info
-    temp_data = np.load(temp_filename, mmap_mode="r", allow_pickle=True)
-    build_folder = temp_filename[:temp_filename.rfind('/')]
-    build_number = build_folder[:build_folder.find('_')]
-    build_number = build_number[(build_number.rfind('/') + 1):]
-
-    # Get frame info
-    num_frames = temp_data.shape[0]
-    height = temp_data[0].shape[0]
-    width = temp_data[0].shape[1]
-
-    if end == 0:
-        end = num_frames - 1
+    if end <= 0 or end >= last_frame:
+        end = last_frame
+    else:
+        end = end + 1
 
     # Make blank image to increment
     threshold_img = np.zeros((height, width), dtype=np.float32)
 
     # Check each pixel, if pixel is over threshold, increment that pixel in theshold_img
-    for i, frame in enumerate(temp_data[start:end]):
-        # Show progress
-        progress_bar.printProgressBar(i,
-                                      num_frames,
-                                      prefix='Generating threshold image...')
-        over_thresh_array = cv2.inRange(frame, threshold, int(np.amax(frame)))
+    start_time = time.time()
+    for i, frame in enumerate(dataset[start:end]):
+        if show_progress:
+            printProgressBar(i - start, end - start, start_time,
+                             "Generating threshold image...")
+        increment_from_thresh(threshold_img, frame, threshold)
 
-        threshold_img += over_thresh_array
+    if cap is not None:
+        over_cap = np.where(threshold_img > cap)
+        for y, x in zip(over_cap[0], over_cap[1]):
+            threshold_img[y, x] = cap
 
+    return threshold_img
+
+
+def save_threshold_img(filename: str,
+                       threshold: int,
+                       dst_folder=None,
+                       start=0,
+                       end=0,
+                       cap=None,
+                       show_progress=False):
+    # Get temp data info
+    temp_data = np.load(filename, mmap_mode="r", allow_pickle=True)
+    build_folder = filename[:filename.rfind('/')]
+    build_number = build_folder[:build_folder.find('_')]
+    build_number = build_number[(build_number.rfind('/') + 1):]
+
+    # If a dst folder is not entered, dst folder is the build folder
     if dst_folder is None:
         dst_folder = build_folder
 
     # Generate a filename based on build_number and threshold used
-    filename = dst_folder + '/' + build_number + '_threshold' + str(
+    save_filename = filename = dst_folder + '/' + build_number + '_threshold' + str(
         threshold) + '.png'
+
+    threshold_img = get_threshold_img(temp_data, threshold, start, end, cap,
+                                      show_progress)
+
     fig, ax = plt.subplots()
     fig.suptitle('Build: ' + str(build_number) + ' Threshold: ' +
                  str(threshold))
@@ -64,7 +81,7 @@ def generate_threshold_image(temp_filename: str,
                               ax=ax,
                               label='Number of frames above ' + str(threshold))
 
-    print(filename)
     plt.savefig(filename)
     plt.close()
+    print('\n')
     print('Threshold img saved as: ' + filename)
