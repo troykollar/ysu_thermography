d457cd94bffaca70f78b72a5a22dbb59d47f964a
Troy Kollar <troykollar@gmail.com>
2020-05-11 17:38:05 -0400

    Added command line arguments that can be set when invoking the function

--- select_pixels.py
+++ select_pixels.py
@@ -1,11 +1,47 @@
 import cv2
+from datetime import datetime
+import numpy as np
+import argparse
+from np_vid_viewer.helper_functions import get_build_number, get_build_folder
+from np_vid_viewer.composite import get_threshold_img
 from Visualization_script import get_visualization
 from bubble_plot_3d import plotBubble
-from datetime import datetime
+
+PARSER = argparse.ArgumentParser(description='Generate plots from a dataset.')
+PARSER.add_argument('temp_data',
+                    type=str,
+                    help='the file location/name of the temperature data')
+PARSER.add_argument('THRESHOLD',
+                    type=int,
+                    help='temperature theshold of the image')
+PARSER.add_argument('-start',
+                    type=int,
+                    default=0,
+                    required=False,
+                    help='First frame to consider.')
+PARSER.add_argument('-end',
+                    type=int,
+                    default=-1,
+                    required=False,
+                    help='Last frame to consider.')
+ARGS = PARSER.parse_args()
+temp_file = ARGS.temp_data
+composite_threshold = ARGS.THRESHOLD
+start_frame = ARGS.start
+end_frame = ARGS.end
+
+temp_data = np.load(temp_file, allow_pickle=True, mmap_mode='r')
+
+img1 = get_threshold_img(temp_data,
+                         composite_threshold,
+                         show_progress=True,
+                         start=start_frame,
+                         end=end_frame)
+img1 = cv2.normalize(img1, img1, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+img1 = cv2.applyColorMap(img1, cv2.COLORMAP_INFERNO)
 
 pixel_locations = []
 corner_locations = []
-redraw = True
 
 select_corners = True
 
@@ -20,9 +56,7 @@ def on_click(event, x, y, flags, param):
             pixel_locations.append((x, y))
 
 
-img1 = cv2.imread("/home/troy/Pictures/4-20_700.png")
-
-window_name = "4-20"
+window_name = get_build_number(temp_file)
 cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
 cv2.setMouseCallback(window_name, on_click)
 
@@ -34,10 +68,14 @@ while True:
 
     draw_img = img1.copy()
 
-    for corner_pixel in corner_locations:
-        corner_x = corner_pixel[0]
-        corner_y = corner_pixel[1]
-        draw_img[corner_y][corner_x] = [0, 255, 255]
+    if select_corners:
+        for corner_pixel in corner_locations:
+            corner_x = corner_pixel[0]
+            corner_y = corner_pixel[1]
+            draw_img[corner_y][corner_x] = [0, 255, 255]
+    else:
+        draw_img = cv2.polylines(draw_img, np.int32([corner_locations]), True,
+                                 (0, 255, 255), 1)
 
     for pixel in pixel_locations:
         pixel_x = pixel[0]
@@ -63,9 +101,7 @@ savetime = str(datetime.now().month) + '-' + str(
     datetime.now().day) + '_' + str(datetime.now().hour) + '-' + str(
         datetime.now().minute)
 
-path = '/media/troy/TroyUSB/thermography/All/Thermal Camera'
-temp_file = '/media/troy/TroyUSB/thermography/All/Thermal Camera/thermal_cam_temps.npy'
-threshold = 200
+path = get_build_folder(temp_file)
 
 if len(corner_locations) == 4:
     if len(pixel_locations) > 0:
@@ -85,6 +121,12 @@ if len(corner_locations) == 4:
             pixel_x = pixel[0]
             pixel_y = pixel[1]
             print('\nGenerating 2D plots for: ' + str(pixel))
-            get_visualization(path, pixel_x, pixel_y, threshold=threshold)
+            get_visualization(temp_data,
+                              path,
+                              pixel_x,
+                              pixel_y,
+                              threshold=composite_threshold,
+                              start=start_frame,
+                              end=end_frame)
             print('\nGenerating 3D plot for: ' + str(pixel))
-            plotBubble(temp_file, pixel, threshold=threshold)
+            plotBubble(temp_data, pixel, threshold=composite_threshold)
