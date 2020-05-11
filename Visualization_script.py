7e7db75138bb59eb64569a31f8f68243b984bc89
Troy Kollar <troykollar@gmail.com>
2020-05-11 16:26:04 -0400

    Added Nathan's updates and added progressbar

--- Visualization_script.py
+++ Visualization_script.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 import math
+from np_vid_viewer.helper_functions import printProgressBar
 
 
 def get_visualization(path,
@@ -10,8 +11,9 @@ def get_visualization(path,
                       data_list='thermal_cam_temps.npy',
                       start=None,
                       end=None,
-                      bins=100,
-                      threshold=100):
+                      threshold=500,
+                      gridlines=True):
+
     path = path  #specify directory to look in
     data_list = data_list  #specify data set to look at
     if start is None:
@@ -24,7 +26,7 @@ def get_visualization(path,
         end_frame = end
 
     start_frame = 0  #specify which frame to begin at
-    num_frame = 27000  #specify number of frames to look at
+    num_frame = 27000  #specify number of frames to look a
 
     directory = os.chdir(path)
     temps = np.load(data_list, mmap_mode='r')
@@ -35,8 +37,7 @@ def get_visualization(path,
     x_mag = []
     y_mag = []
 
-    #Angle arrays
-    angle_arr_rad = []
+    #Angle array
     angle_arr_deg = []
 
     #Magnitude array
@@ -46,7 +47,7 @@ def get_visualization(path,
     record = []
 
     for frame_index in vid_frames:
-        print(str(frame_index) + '/' + str(vid_frames))
+        printProgressBar(frame_index, vid_frames[-1])
 
         pixel = temps[frame_index, pixel_x, pixel_y]
         data = temps[frame_index, :, :]
@@ -66,59 +67,102 @@ def get_visualization(path,
             #Angle Calculation
             angle_rad = (np.arctan2(y_dir, x_dir) - (math.pi / 2)
                          )  #shift -90 deg
-            angle_deg = (angle_rad * (180 / math.pi))
+            angle_deg = (angle_rad * (180 / math.pi))  #Convert to degrees
 
             x_mag.append(x_dir)  #store pixel x-direction for frame in array
             y_mag.append(y_dir)  #store pixel y-direction for frame in array
             mag_arr.append(
                 magnitude)  #store pixel magnitude for frame in array
-            angle_arr_rad.append(
-                angle_rad)  #store pixel angle for frame in array
             angle_arr_deg.append(
                 angle_deg)  #store pixel angle for frame in array
 
-    #Plotting----------------------------------------------------------------------
-    fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(16, 12))
-    fig.tight_layout()
-    fig.suptitle(
-        'Pixel ({},{}) Temperature Data: {} bins, Threshold {}'.format(
-            pixel_x, pixel_y, bins, threshold))
-
-    #Magnitude plot
-    mag_minimum = np.amin(mag_arr)
-    mag_maximum = np.amax(mag_arr)
-    ax1.set_xlabel('Magnitude (sqrt(x^2+y^2))')
-    ax1.set_ylabel('Frequency')
-    ax1.hist(mag_arr,
-             bins=bins,
-             range=(mag_minimum, mag_maximum),
-             edgecolor='black')
-
-    #Angle plot - degrees
-    angle_deg_minimum = np.amin(angle_arr_deg)
-    angle_deg_maximum = np.amax(angle_arr_deg)
-    ax2.set_xlabel('Angle (°)')
-    ax2.set_ylabel('Frequency')
-    ax2.hist(angle_arr_deg,
-             bins=bins,
-             range=(angle_deg_minimum, angle_deg_maximum),
-             edgecolor='black')
-
-    #2D Histogram - Magnitude vs Angle (degrees)
-    ax3.set_xlabel('Angle (°)')
-    ax3.set_ylabel('Magnitude (sqrt(x^2+y^2))')
-    ax3.hist2d(angle_arr_deg, mag_arr, bins=(30, 20), cmap=plt.cm.Reds)
-
-    #"Bubble Plot" - Magnitude vs Angle (degrees)
-    ax4.set_xlabel('Angle (°)')
-    ax4.set_ylabel('Magnitude (sqrt(x^2+y^2))')
-    ax4.scatter(angle_arr_deg, mag_arr, alpha=0.5)
-
-    plt.savefig('pixel' + str(pixel_x) + '-' + str(pixel_y) + '_visualization')
-
     data_points = len(record)
-    hottest = np.amax(record)
 
-    print('Data for frame {} to {}'.format(start_frame, end_frame))
-    print('Number of data points above {}: {}'.format(threshold, data_points))
-    print('Highest Value: {}'.format(hottest))
+    if not (x_mag or y_mag or mag_arr or angle_arr_rad or angle_arr_deg):
+        print('Data array is empty!')
+        print(
+            'There are {} data points above threshold of {} within frames {} to {}'
+            .format(data_points, threshold, start_frame, num_frame))
+
+    else:
+        #Plotting
+        #Magnitude plot
+        mag_minimum = np.amin(mag_arr)
+        mag_maximum = np.amax(mag_arr)
+        binning1 = int((abs(mag_maximum - mag_minimum)) / 5)
+        fig1, ax1 = plt.subplots()
+        fig1.suptitle(
+            'Pixel ({},{}) Magnitude Histogram:\n{} Bins, Threshold: {}'.
+            format(pixel_x, pixel_y, binning1, threshold))
+        ax1.set_xlabel('Magnitude (sqrt(x^2+y^2))')
+        ax1.set_ylabel('Frequency')
+        ax1.hist(mag_arr,
+                 bins=binning1,
+                 range=(mag_minimum, mag_maximum),
+                 edgecolor='black')
+        ax1.grid(b=gridlines, which='major', alpha=0.3)
+
+        #Angle plot - degrees
+        angle_deg_minimum = np.amin(angle_arr_deg)
+        angle_deg_maximum = np.amax(angle_arr_deg)
+        binning2 = int(binning1 / 1.5)
+        fig2, ax2 = plt.subplots()
+        fig2.suptitle(
+            'Pixel ({},{}) Angle Histogram:\n{} Bins, Threshold: {}\n'.format(
+                pixel_x, pixel_y, binning2, threshold))
+        ax2.set_xlabel('Angle (°)')
+        ax2.set_ylabel('Frequency')
+        counts, bins, bars = ax2.hist(angle_arr_deg,
+                                      bins=binning2,
+                                      range=(angle_deg_minimum,
+                                             angle_deg_maximum),
+                                      edgecolor='black')
+        ax2.grid(b=gridlines, which='major', alpha=0.3)
+
+        #2D Histogram - Magnitude vs Angle (degrees)
+        histgrid = (binning1, binning2)
+        fig3, ax3 = plt.subplots()
+        fig3.suptitle(
+            'Pixel ({},{}) Magnitude vs Angle Histogram:\nThreshold: {}\n'.
+            format(pixel_x, pixel_y, threshold))
+        ax3.set_xlabel('Angle (°)')
+        ax3.set_ylabel('Magnitude (sqrt(x^2+y^2))')
+        histplot = ax3.hist2d(angle_arr_deg,
+                              mag_arr,
+                              bins=histgrid,
+                              cmap='gnuplot')
+        ax3.grid(b=gridlines, which='major', alpha=0.3)
+        plt.colorbar(histplot[3])
+
+        #Scatterplot - Magnitude vs Angle (degrees)
+        fig4, ax4 = plt.subplots()
+        fig4.suptitle(
+            'Pixel ({},{}) Magnitude vs Angle Scatterplot:\nThreshold: {}\n'.
+            format(pixel_x, pixel_y, threshold))
+        ax4.set_xlabel('Angle (°)')
+        ax4.set_ylabel('Magnitude (sqrt(x^2+y^2))')
+        ax4.scatter(angle_arr_deg, mag_arr)
+        ax4.grid(b=gridlines, which='major', alpha=0.3)
+
+        #"Hexbin plot" - Magnitude vs Angle (degrees)
+        hexgrid = (binning1, binning2)
+        fig5, ax5 = plt.subplots()
+        fig5.suptitle(
+            'Pixel ({},{}) Magnitude and Angle Hist:\nThreshold: {}\n'.format(
+                pixel_x, pixel_y, threshold))
+        ax5.set_xlabel('Angle (°)')
+        ax5.set_ylabel('Magnitude (sqrt(x^2+y^2))')
+        hexplot = ax5.hexbin(angle_arr_deg,
+                             mag_arr,
+                             gridsize=hexgrid,
+                             cmap='gnuplot')
+        ax5.grid(b=gridlines, which='major', alpha=0.3)
+        plt.colorbar(hexplot)
+
+        plt.show()
+
+        hottest = np.amax(record)
+        print('Data for frame {} to {}'.format(start_frame, end_frame))
+        print('Number of data points above {}: {}'.format(
+            threshold, data_points))
+        print('Highest Value: {}'.format(hottest))
