1cfdc10fbe020107744dbdb895db819c9c347298
Troy Kollar <troykollar@gmail.com>
2020-05-14 17:57:52 -0400

    Added build_folder attribute and frame scaling as it is returned

--- dataset.py
+++ dataset.py
@@ -17,6 +17,7 @@ class DataSet:
 
         # Load thermal cam temp data
         self.temp_fname = temps_file
+        self.build_folder = self.get_build_folder()
         self.frame_data = np.load(self.temp_fname,
                                   mmap_mode='r',
                                   allow_pickle=True)
@@ -32,6 +33,7 @@ class DataSet:
                     self.remove_top(frame)
                 if remove_bottom_reflection:
                     self.remove_bottom(frame)
+
                 self.cleaned_frame_data[i] = frame
 
         self.shape = self.frame_data.shape
@@ -44,6 +46,9 @@ class DataSet:
             frame = self.cleaned_frame_data[index]
         else:
             frame = self.frame_data[index]
+
+        if self.scale_factor != 1:
+            frame = self.scale_frame(frame)
         return frame
 
     def remove_top(self, frame: np.ndarray):
@@ -96,6 +101,17 @@ class DataSet:
         # Draw a line at bottom of piece (for debugging)
         #cv2.line(frame, (0, y), (frame.shape[1], y), int(np.amax(frame)), 1)
 
+    def scale_frame(self, frame: np.ndarray):
+        width = int(frame.shape[1] * self.scale_factor)
+        height = int(frame.shape[0] * self.scale_factor)
+        size = (width, height)
+        frame = cv2.resize(frame, size, interpolation=cv2.INTER_LINEAR)
+        return frame
+
+    def get_build_folder(self):
+        build_folder = self.temp_fname[:self.temp_fname.rfind('/')]
+        return build_folder
+
 
 def get_dataset_CLargs(parser: argparse.ArgumentParser):
     """Add dataset related CL arguments to given parser.
@@ -108,6 +124,8 @@ def get_dataset_CLargs(parser: argparse.ArgumentParser):
         0 or 1 specifying whether or not to remove top reflections.
     bot: optional
         0 or 1 specifying whether or not to remove bottom reflections.
+    scale: optional
+        int specifying the factor to scale frames by.
     """
     parser.add_argument('temp_data',
                         type=str,
@@ -126,6 +144,10 @@ def get_dataset_CLargs(parser: argparse.ArgumentParser):
         help=
         '0 or 1 specifying whether or not to remove bottom reflections from dataset.'
     )
+    parser.add_argument('-scale',
+                        type=int,
+                        default=1,
+                        help='Factor to scale frames by.')
 
 
 if __name__ == '__main__':
@@ -139,14 +161,17 @@ if __name__ == '__main__':
     test_file = args.temp_data
     top = bool(args.top)
     bot = bool(args.bot)
+    scale = args.scale
 
     dset = DataSet(test_file,
                    remove_top_reflection=top,
-                   remove_bottom_reflection=bot)
+                   remove_bottom_reflection=bot,
+                   scale_factor=scale)
     for frame in dset:
         frame = cv2.normalize(frame, frame, 0, 255, cv2.NORM_MINMAX,
                               cv2.CV_8UC1)
         frame = cv2.applyColorMap(frame, cv2.COLORMAP_INFERNO)
+        cv2.namedWindow('Frame', cv2.WINDOW_NORMAL)
         cv2.imshow('Frame', frame)
         cv2.waitKey(1)
     cv2.destroyAllWindows()
